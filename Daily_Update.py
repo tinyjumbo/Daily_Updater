0cb23f02272b17e18a8b6d0afa7bd4119db2a97d
zhihui <zhihui.qiu.tufts@gmail.com>
2016-03-14 23:05:25 -0400

    code style

--- Daily_Update.py
+++ Daily_Update.py
@@ -21,68 +21,60 @@ client = MongoClient()
 client = MongoClient('mongodb://162.243.122.37:27017/')
 
 #sample the tweets 1/sample
-sample=100
-timeslot=10
+SAMPLE = 100
+TIMESLOT = 10
 
-
-
-def Count_Update(company,score):
-	query_collection= getattr(client.tinyjumbo,company)
+#update date and score infomation in MongoDB
+def Count_Update(company, score):
+	query_collection = getattr(client.tinyjumbo,company)
 	update_collection = client.tinyjumbo.tcount
 	end = datetime.datetime.now()
 	start = end - timedelta(1)
-
-	#print query_collection.find_one()
 	count = query_collection.find({'time': {'$gte': str(start), '$lt': str(end)}}).count()
-	record={"company":company,"count":count,"date":datetime.datetime.now(),"score":score}
-	result=update_collection.insert_one(record)
-	#print result.inserted_id
+	record = {"company":company,"count":count,"date":datetime.datetime.now(),"score":score}
+	result = update_collection.insert_one(record)
+	return
 
+#read csv and modify date for later process
 def read(a):
-    tweet=pd.read_csv(a)
-    date=tweet['time']
-    text=tweet['text']
-    day=pd.to_datetime(date,format='%Y-%m-%d %H:%M:%S.%f')
-    day=day.map(lambda x: x.strftime('%Y-%m-%d'))
+    tweet = pd.read_csv(a)
+    date = tweet['time']
+    text = tweet['text']
+    day = pd.to_datetime(date,format = '%Y-%m-%d %H:%M:%S.%f')
+    day = day.map(lambda x: x.strftime('%Y-%m-%d'))
     text = map(lambda x: [x], text)
-    tweet=pd.DataFrame(text,index=day,columns=['text'])
+    tweet = pd.DataFrame(text, index = day, columns = s['text'])
     return tweet
 
+#caculate score by using senti_classifier
 def sentiment_score(dataset,sample):
-	count=pos_sum=neg_sum=0
+	count = pos_sum = neg_sum = 0
 	for sentence in dataset:
 		if count%sample==0:
 			pos_score, neg_score = senti_classifier.polarity_scores([sentence])
 			print "pos_score: " + str(pos_score) + "  neg_score" + str(neg_score)
-			pos_sum+=pos_score
-			neg_sum+=neg_score
-		count+=1
-	pos_score,neg_score=pos_sum/(pos_sum+neg_sum),neg_sum/(pos_sum+neg_sum)	
+			pos_sum += pos_score
+			neg_sum += neg_score
+		count += 1
+	pos_score,neg_score = pos_sum/(pos_sum + neg_sum),neg_sum/(pos_sum + neg_sum)	
 	return pos_score-neg_score
 
-
+#mian function
 def query_and_process(company):
-	query="mongoexport -h 162.243.122.37:27017 -d tinyjumbo -c "\
-			 +company+ " --type=csv --fields time,text -q '{ \"time\": { $gt: \"2016-03\", $lt: \"2016-03-13 24:00\" } }' --out "\
-			 +company+".csv"
+	query = "mongoexport -h 162.243.122.37:27017 -d tinyjumbo -c "\
+			 + company + " --type=csv --fields time,text -q '{ \"time\": { $gt: \"2016-03\", $lt: \"2016-03-13 24:00\" } }' --out "\
+			 + company + ".csv"
 	os.system(query)
-	time.sleep(timeslot)
-	tweet=read(company+".csv")
-	sub=tweet.loc[str(datetime.datetime.now().date())]
-	score=sentiment_score(sub['text'],sample)
-
+	time.sleep(TIMESLOT)
+	tweet = read(company + ".csv")
+	sub = tweet.loc[str(datetime.datetime.now().date())]
+	score = sentiment_score(sub['text'],SAMPLE)
 	Count_Update(company,score)
-
-	os.system("rm -f "+company+".csv")	
-
-
+	os.system("rm -f "+ company + ".csv")	
 	return
-
 
 #save daily tweets as csv
 query_and_process("google")
 query_and_process("amazon")
 query_and_process("facebook")
-
 print "finished"
-
