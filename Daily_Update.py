e97643b22c89e7bfd64a8d5fc30a22487627e338
zhihui <zhihui.qiu.tufts@gmail.com>
2016-03-28 21:21:23 -0400

    Stable version for daily update

--- Daily_Update.py
+++ Daily_Update.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-this small program is to count the number of tweets of each company
+This small program is to count the number of tweets of each company
 update the number and score in tcount collection for each company
 '''
 
@@ -15,27 +15,28 @@ from senti_classifier import senti_classifier
 import time
 import os
 
-
-#Mongo config
+# Mongo config
 client = MongoClient()
 client = MongoClient('mongodb://162.243.122.37:27017/')
 
-#sample the tweets 1/sample
+# Sample the tweets 1/sample
 SAMPLE = 100
 TIMESLOT = 10
+CURRDATE = str(datetime.datetime.now().date())
 
-#update date and score infomation in MongoDB
+# Update date and score infomation in MongoDB
 def Count_Update(company, score):
-	query_collection = getattr(client.tinyjumbo,company)
-	update_collection = client.tinyjumbo.tcount
-	end = datetime.datetime.now()
-	start = end - timedelta(1)
-	count = query_collection.find({'time': {'$gte': str(start), '$lt': str(end)}}).count()
-	record = {"company":company,"count":count,"date":datetime.datetime.now(),"score":score}
-	result = update_collection.insert_one(record)
-	return
+    query_collection = getattr(client.tinyjumbo,company)
+    update_collection = client.tinyjumbo.tcount
+    end = CURRDATE + " 24" 
+    start = CURRDATE
+    count = query_collection.find({'time': {'$gte': start, '$lt': end}}).count()
+    record = {"company":company,"count":count,"date":CURRDATE,"score":score}
+    result = update_collection.insert_one(record)
+    return
 
-#read csv and modify date for later process
+
+# Read csv and modify date for later process
 def read(a):
     tweet = pd.read_csv(a)
     date = tweet['time']
@@ -46,38 +47,51 @@ def read(a):
     tweet = pd.DataFrame(text, index = day, columns = ['text'])
     return tweet
 
-#caculate score by using senti_classifier
+
+# Caculate score by using senti_classifier
 def sentiment_score(dataset, sample):
-	count = pos_sum = neg_sum = 0
-	for sentence in dataset:
-		if count%sample==0:
-			pos_score, neg_score = senti_classifier.polarity_scores([sentence])
-			print "pos_score: " + str(pos_score) + "  neg_score" + str(neg_score)
-			pos_sum += pos_score
-			neg_sum += neg_score
-		count += 1
-	sum_val = pos_sum + neg_sum
-	pos_score,neg_score = pos_sum/min(0.0000001,sum_val),neg_sum/min(0.0000001,sum_val)	
-	return pos_score - neg_score
+    count = pos_sum = neg_sum = 0
+    for sentence in dataset:
+        if count%sample==0:
+            pos_score, neg_score = senti_classifier.polarity_scores([sentence])
+            print "pos_score: " + str(pos_score) + "  neg_score" + str(neg_score)
+            pos_sum += pos_score
+            neg_sum += neg_score
+        count += 1
+    sum_val = pos_sum + neg_sum
+    pos_score,neg_score = pos_sum/max(0.0000001,sum_val),neg_sum/max(0.0000001,sum_val)    
+    return pos_score - neg_score
 
-#mian function
+
+# Mian function
 def query_and_process(company):
-	query = "mongoexport -h 162.243.122.37:27017 -d tinyjumbo -c "
-			 + company + " --type=csv --fields time,text -q '{ \"time\": { $gt: \"" 
-			 + str(datetime.datetime.now().date()) + "\", $lt: \"" 
-			 + str(datetime.datetime.now().date()) + " 24:00\" } }' --out "
-			 + company + ".csv"
-	os.system(query)
-	time.sleep(TIMESLOT)
-	tweet = read(company + ".csv")
-	sub = tweet.loc[str(datetime.datetime.now().date())]
-	score = sentiment_score(sub['text'],SAMPLE)
-	Count_Update(company,score)
-	os.system("rm -f "+ company + ".csv")	
-	return
+    # Query data
+    query = "mongoexport -h 162.243.122.37:27017 -d tinyjumbo -c " + company + " --type=csv --fields time,text -q '{ \"time\": { $gt: \"" + CURRDATE + "\", $lt: \"" + CURRDATE + " 24:00\" } }' --out " + company + ".csv"
+    os.system(query)
+    time.sleep(TIMESLOT)
+    # Read data
+    tweet = read(company + ".csv")
+    sub = tweet.loc[CURRDATE]
+    # Get score
+    score = sentiment_score(sub['text'],SAMPLE)
+    # Update DB
+    Count_Update(company,score)
+    os.system("rm -f "+ company + ".csv")    
+    return
 
-#save daily tweets as csv
-query_and_process("google")
-query_and_process("amazon")
-query_and_process("facebook")
+
+# Save daily tweets as csv
+names = ["google", "amazon", "facebook"]
+map(query_and_process, names)
 print "finished"
+
+''' if want to caculate mutiple days. Use following:
+hard_code_time = '2016-03-20'
+timearry = [hard_code_time[:-2]+str(int(hard_code_time[-2:])+i) for i in range(13) ]
+
+for t in timearry:
+    CURRDATE = t
+    print CURRDATE
+    map(query_and_process, names)
+    print "finished"
+'''
